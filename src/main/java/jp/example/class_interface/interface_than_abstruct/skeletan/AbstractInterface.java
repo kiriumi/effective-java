1b90e9f4a9f3fc5b022c953d5a65fe182bbfb87e
kiriumi <kiriumi.miko@gmail.com>
2021-09-25 23:25:57 +0900

    Javadocに実装要件を追加

--- src/main/java/jp/example/class_interface/interface_than_abstruct/skeletan/AbstractInterface.java
+++ src/main/java/jp/example/class_interface/interface_than_abstruct/skeletan/AbstractInterface.java
@@ -5,6 +5,13 @@ import lombok.Getter;
 import lombok.ToString;
 
 // Objectクラスのメソッドをオーバーライドする場合は、骨格実装する
+/**
+ *
+ * @implSpec
+ * start()とend()をオーバーライドして処理を実装すること<br>
+ * mainメソッドを実行した際、start() -> end()の順で実行される
+ *
+ */
 @ToString
 @EqualsAndHashCode
 public abstract class AbstractInterface {
