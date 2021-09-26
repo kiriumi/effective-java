8b69698a26264da7cb7a991268c2cca25e322b2c
kiriumi <kiriumi.miko@gmail.com>
2021-09-26 14:47:52 +0900

    スコープの見直し

--- src/main/java/jp/example/generics/container/Container.java
+++ src/main/java/jp/example/generics/container/Container.java
@@ -5,17 +5,17 @@ import java.util.Map;
 import java.util.Objects;
 
 // 異種コンテナ：クラスをキー、インスタンスを値とするマップ
-public class Container {
+class Container {
 
-    Map<Class<?>, Object> countainer = new HashMap<>();
+    private final Map<Class<?>, Object> countainer = new HashMap<>();
 
-    public <T> void put(Class<T> type, T instance) {
+    <T> void put(Class<T> type, T instance) {
 
         // instance.getClass()だとProxyクラス（jdk.proxy2.$Proxy1）のため、キーとして不適切になってしまう
         countainer.put(Objects.requireNonNull(type), type.cast(instance));
     }
 
-    public <T> T get(Class<T> type) {
+    <T> T get(Class<T> type) {
 
         // type.castメソッドだと「@SuppressWarnings("unchecked")」を付けなくてよい
         // ※(T) だと必要
