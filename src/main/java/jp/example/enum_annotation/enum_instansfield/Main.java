6eb185ee7fb0453c760d4194f3e4f162c043f859
kiriumi <kiriumi.miko@gmail.com>
2021-09-26 15:49:35 +0900

    コメントを追加

--- src/main/java/jp/example/enum_annotation/enum_instansfield/Main.java
+++ src/main/java/jp/example/enum_annotation/enum_instansfield/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // 序数を使用した場合
         System.out.println(ExampleBad.SOLO.number());
+
+        // インスタンスフィールドを使用した場合
         System.out.println(ExampleGood.SOLO.number());
 
     }
